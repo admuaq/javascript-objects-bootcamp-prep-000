49b0d1c954adeb57f7a1a4de16e45aabde47e89a
Adham Muhammad <admuaq@gmail.com>
2018-06-22 10:39:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artist: "Hey", song: "whats up"};
 
 function updatePlayList (playlist, artistName, songTitle) {
-  return playlist.assign({}, {artistName: ""},{songTitle: ""});
+  return playlist.assign({}, {artistName: "songTitle"});
 }

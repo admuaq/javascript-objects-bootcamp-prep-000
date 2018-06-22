8d63b447bb85b715b5b38db7cddc346c9e5337df
Adham Muhammad <admuaq@gmail.com>
2018-06-22 10:56:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist: "Hey", song: "whats up"};
 
 function updatePlayList (playlist, artistName, songTitle) {
-  playlist['artistName'] = 'songTitle';
+  playlist.artistName = 'songTitle';
   return playlist;
 }
